ae54e5640c64b5acfea5e6679a2f8946b6be1777
Sergii Illiukhin <sil@ciklum.com>
2016-03-24 08:49:00 +0200

    Tests added

---
+++ Tests/RadaOnline.Queries.Tests/Council/CouncilOverviewQueryTests.cs
@@ -0,0 +1,34 @@
+﻿namespace RadaOnline.Queries.Tests.Council
+{
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using Moq;
+
+    using Ploeh.AutoFixture;
+
+    using RadaOnline.Database.Repositories.Interfaces;
+    using RadaOnline.Queries.Council;
+
+    [TestClass]
+    public class CouncilOverviewQueryTests
+    {
+        [TestMethod]
+        public void Execute_Should_Call_CouncilRepository_Overview()
+        {
+            //Arrange
+            var repositoryMock = new Mock<ICouncilRepository>();
+            var query = new CouncilOverviewQuery(repositoryMock.Object);
+            var fixture = new Fixture();
+            var name = fixture.Create<string>();
+            var take = fixture.Create<int>();
+            var skip = fixture.Create<int>();
+
+            //Act
+            query.Execute(name, take, skip);
+
+            //Assert
+            repositoryMock.Verify(x => x.Overview(name, take, skip), Times.Once);
+        }
+    }
+}
---
+++ Tests/RadaOnline.Queries.Tests/Council/CouncilRetrieveQueryTests.cs
@@ -0,0 +1,31 @@
+﻿namespace RadaOnline.Queries.Tests.Council
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using Moq;
+
+    using Ploeh.AutoFixture;
+
+    using RadaOnline.Database.Repositories.Interfaces;
+    using RadaOnline.Queries.Council;
+
+    [TestClass]
+    public class CouncilRetrieveQueryTests
+    {
+        [TestMethod]
+        public void Execute_Should_Call_CouncilRepository_Retrieve()
+        {
+            //Arrange
+            var repositoryMock = new Mock<ICouncilRepository>();
+            var query = new CouncilRetrieveQuery(repositoryMock.Object);
+            var fixture = new Fixture();
+            var id = fixture.Create<int>();
+
+            //Act
+            query.Execute(id);
+
+            //Assert
+            repositoryMock.Verify(x => x.Retrieve(id), Times.Once);
+        }
+    }
+}
