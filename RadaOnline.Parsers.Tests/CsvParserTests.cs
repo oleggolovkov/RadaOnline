f4a46b190cf9e0ea9ee323a03cc161ede3727d02
oleg <oleggolovkov@gmail.com>
2016-03-26 15:39:57 +0200

    csv parser

---
+++ RadaOnline.Parsers.Tests/CsvParserTests.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Linq;
+
+namespace RadaOnline.Parsers.Tests {
+    [TestClass]
+    public class CsvParserTests {
+        [TestMethod]
+        public void Session_9() {
+            var reader = new StreamReader("Data\\Поіменні голосування БМР - 9 сесія.csv");
+            var parser = new CsvParser();
+            var session = parser.Parse(reader);
+
+            //session properties
+            Assert.IsNotNull(session);
+            Assert.AreEqual(9, session.Number);
+            Assert.IsFalse(session.IsRegular);
+
+
+            //session items
+            Assert.AreEqual(5, session.SessionItems.Count);
+            string[] expectedTitles = {
+                "Порядок",
+                "прийняття",
+                "співфінансування",
+                "культури",
+                "Заслуховування"
+            };
+            var titles = string.Join("", session.SessionItems.Select(si => si.Title));
+            Assert.IsTrue(expectedTitles.All(t => titles.Contains(t)));
+            var sessionItems = session.SessionItems.ToArray();
+            Assert.AreEqual(Database.Models.Enums.SessionItemStatus.Accepted, sessionItems[0].Status);
+            Assert.AreEqual(Database.Models.Enums.SessionItemStatus.Rejected, sessionItems[1].Status);
+            Assert.AreEqual(Database.Models.Enums.SessionItemStatus.Accepted, sessionItems[2].Status);
+            Assert.AreEqual(Database.Models.Enums.SessionItemStatus.Rejected, sessionItems[3].Status);
+            Assert.AreEqual(Database.Models.Enums.SessionItemStatus.Accepted, sessionItems[4].Status);
+
+
+            //decisions
+            var decisions = session.SessionItems.SelectMany(si => si.Decisions).ToArray();
+            Assert.AreEqual(12, decisions.Length);
+            string[] expectedDescriptions = {
+                "Порядок",
+                "Внесення",
+                "денний",
+                "прийняття",
+                "рішення",
+                "співфінансування",
+                "проекту",
+                "культури",
+                "Заслухавши",
+                "Заслуховування",
+                "молоді",
+                "начальника",
+            };
+            var descriptions = string.Join("", session.SessionItems.SelectMany(si => si.Decisions).Select(d=>d.Description));
+            Assert.IsTrue(expectedDescriptions.All(d => descriptions.Contains(d)));
+            Assert.IsTrue(decisions[0].IsAccepted);
+            Assert.IsFalse(decisions[1].IsAccepted);
+            Assert.IsTrue(decisions[2].IsAccepted);
+            Assert.IsTrue(decisions[3].IsAccepted);
+            Assert.IsFalse(decisions[4].IsAccepted);
+            Assert.IsTrue(decisions[5].IsAccepted);
+            Assert.IsTrue(decisions[6].IsAccepted);
+            Assert.IsTrue(decisions[7].IsAccepted);
+            Assert.IsFalse(decisions[8].IsAccepted);
+            Assert.IsFalse(decisions[9].IsAccepted);
+            Assert.IsTrue(decisions[10].IsAccepted);
+            Assert.IsTrue(decisions[11].IsAccepted);
+            
+            Assert.AreEqual(Database.Models.Enums.DecitionType.Base, decisions[0].Type);
+            Assert.AreEqual(Database.Models.Enums.DecitionType.Changes, decisions[1].Type);
+            Assert.AreEqual(Database.Models.Enums.DecitionType.Final, decisions[2].Type);
+            Assert.AreEqual(Database.Models.Enums.DecitionType.Base, decisions[3].Type);
+            Assert.AreEqual(Database.Models.Enums.DecitionType.Final, decisions[4].Type);
+            Assert.AreEqual(Database.Models.Enums.DecitionType.Base, decisions[5].Type);
+            Assert.AreEqual(Database.Models.Enums.DecitionType.Final, decisions[6].Type);
+            Assert.AreEqual(Database.Models.Enums.DecitionType.Base, decisions[7].Type);
+            Assert.AreEqual(Database.Models.Enums.DecitionType.Changes, decisions[8].Type);
+            Assert.AreEqual(Database.Models.Enums.DecitionType.Final, decisions[9].Type);
+            Assert.AreEqual(Database.Models.Enums.DecitionType.Base, decisions[10].Type);
+            Assert.AreEqual(Database.Models.Enums.DecitionType.Final, decisions[11].Type);
+
+
+            //votes
+            var votes = decisions[0].Votes.ToArray();
+            Assert.AreEqual(37, votes.Length);
+            var pro = votes.Where(v => v.Type == Database.Models.Enums.VoteType.Pro).Count();
+            Assert.AreEqual(30,pro);
+            var contra = votes.Where(v => v.Type == Database.Models.Enums.VoteType.Contra).Count();
+            Assert.AreEqual(3, contra);
+            votes = decisions[11].Votes.ToArray();
+            Assert.AreEqual(37, votes.Length);
+            pro = votes.Where(v => v.Type == Database.Models.Enums.VoteType.Pro).Count();
+            Assert.AreEqual(28, pro);
+            contra = votes.Where(v => v.Type == Database.Models.Enums.VoteType.Contra).Count();
+            Assert.AreEqual(0, contra);
+
+
+            //votes by name
+            votes = decisions.Select(d => d.Votes.First()).ToArray();// SPZH votes
+            Assert.AreEqual(Database.Models.Enums.VoteType.Pro, votes[0].Type);
+            Assert.AreEqual(Database.Models.Enums.VoteType.Contra, votes[1].Type);
+            Assert.AreEqual(Database.Models.Enums.VoteType.Pro, votes[2].Type);
+            Assert.AreEqual(Database.Models.Enums.VoteType.Pro, votes[3].Type);
+            Assert.AreEqual(Database.Models.Enums.VoteType.Contra, votes[4].Type);
+            Assert.AreEqual(Database.Models.Enums.VoteType.Pro, votes[5].Type);
+            Assert.AreEqual(Database.Models.Enums.VoteType.Pro, votes[6].Type);
+            Assert.AreEqual(Database.Models.Enums.VoteType.Pro, votes[7].Type);
+            Assert.AreEqual(Database.Models.Enums.VoteType.Neutral, votes[8].Type);
+            Assert.AreEqual(Database.Models.Enums.VoteType.Pro, votes[9].Type);
+            Assert.AreEqual(Database.Models.Enums.VoteType.Pro, votes[10].Type);
+            Assert.AreEqual(Database.Models.Enums.VoteType.Pro, votes[11].Type);
+        }
+    }
+}
---
+++ RadaOnline.Parsers/CsvParser.cs
@@ -0,0 +1,99 @@
+﻿using RadaOnline.Database.Models;
+using RadaOnline.Database.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RadaOnline.Parsers {
+
+    public class CsvParser {
+        private const int INVALID_VALUE = -1;
+        private const int VOTES_FOR_ACCEPTANCE = 19;
+        private Councilman[] councilmen;
+        private Councilman GetCouncilmenById(int id) { //should be replaced with real implementation
+            return councilmen.First(c => c.Id == id);
+        }
+
+        public Session Parse(TextReader reader) {
+            var session = new Session { SessionItems = new List<SessionItem>()};
+            var title = reader.ReadLine().Trim(',');//first line
+            session.IsRegular = GetIsRegular(title);
+            session.Number = GetNumberFromString(title);
+            reader.ReadLine();//should be an empty string
+            var headers = reader.ReadLine().Split(',');//№ рішення,Проект рішення, Зареєстр., Відсутні, За, Проти, Утрим., Не голос.,М.г.Сапожко І.В.,Секр.Здоровець О.М., ... all other councilmen                      
+
+            //---next block adds demo councilmen. They should be fetched from db 
+            var councilmanId = 0;
+            councilmen = headers.Skip(8).Select(name => new Councilman { FullName = name, Id = councilmanId++}).ToArray();
+
+            var columnsCount = 45; //table should contain 45 columns
+            string sessionItemStr = null;
+            SessionItem sessionItem = null;
+            while ((sessionItemStr = reader.ReadLine()) != null) {
+                var subStrings = sessionItemStr.Split(',');
+                var subStringsCount = subStrings.Length;
+                int number = INVALID_VALUE;
+                var descriptionColumnSpan = subStringsCount - columnsCount + 1; //description can contain commas, this is an easy hack to determine its width   
+                var description = string.Join(",", subStrings.Skip(1).Take(descriptionColumnSpan)).Trim('\"');
+                if (sessionItem==null ||  int.TryParse(subStrings[0], out number)) { //new session item
+                    sessionItem = new SessionItem { Decisions = new List<Decision>(), Title = description.Replace("за основу", "")};                    
+                    sessionItem.Number = number;                    
+                    session.SessionItems.Add(sessionItem);
+                }
+                var decision = new Decision { Votes = new List<Vote>() };
+                decision.Description = description;
+                decision.Type = GetDecisionType(description);
+                subStrings = subStrings.Skip(1 + descriptionColumnSpan).ToArray();//move to next fields
+                var registered = GetNumberFromString(subStrings[0]);
+                var absent = GetNumberFromString(subStrings[1]);
+                var pro = GetNumberFromString(subStrings[2]);
+                var contra = GetNumberFromString(subStrings[3]);
+                var neutral = GetNumberFromString(subStrings[4]);
+                var didNotVote = GetNumberFromString(subStrings[5]);
+                decision.IsAccepted = pro >= VOTES_FOR_ACCEPTANCE;
+                if (decision.Type == DecitionType.Final) {
+                    sessionItem.Status = decision.IsAccepted ? SessionItemStatus.Accepted : SessionItemStatus.Rejected;
+                }        
+                for (int i = 0; i < 37; i++) {
+                    var vote = new Vote();
+                    vote.Type = GetVoteType(subStrings[i + 6]); //councilmen start from position '6' in an adjusted array
+                    vote.Councilman = GetCouncilmenById(i);
+                    decision.Votes.Add(vote);
+                }
+                sessionItem.Decisions.Add(decision);}
+            return session;
+        }
+
+        private int GetNumberFromString(string s) {
+            int number;
+            var numberStr = new string(s.Where(char.IsDigit).ToArray());
+            return int.TryParse(numberStr, out number) ? number : INVALID_VALUE;
+        }
+
+        private bool GetIsRegular(string sessionTitle) {
+            return !sessionTitle.Contains("позачергова");
+        }
+
+        private DecitionType GetDecisionType(string description) {
+            description = description.ToLower();
+            if (description.EndsWith("за основу"))
+                return DecitionType.Base;
+            if (description.EndsWith("в цілому"))
+                return DecitionType.Final;
+            return DecitionType.Changes;
+        }
+
+        private VoteType GetVoteType(string voteString) {
+            if (voteString == "за")
+                return VoteType.Pro;
+            if (voteString == "проти")
+                return VoteType.Contra;
+            if (voteString == "утр.")
+                return VoteType.Neutral;
+            return VoteType.Neutral; //I guess we should add something for 'did not vote'?
+        }
+    }
+}
